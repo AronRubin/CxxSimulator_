ba1859df03b98d92b37b09c56a91147ccd8627e5
Aron Rubin <aronrubin@gmail.com>
2019-07-01 10:24:05 -0400

    Added instancing to the instancing test.

--- src/CxxSimulatorTest.cpp
+++ src/CxxSimulatorTest.cpp
@@ -45,36 +45,63 @@ TEST_F( SimulatorTest, model_adoption ) {
   EXPECT_TRUE( sim::Simulator::getInstance().model( "EmptyModel" ) );
 }
 
-class LoopbackModel : public sim::Model {
-public:
-  LoopbackModel() : Model( "LoopbackModel" ) {
-    addActivitySpec( {
-        "sink",
-        []( sim::Instance &instance, sim::Activity &activity ) {
-          while( activity.state() == sim::Activity::State::RUN ) {
-            activity.padReceive( "in" );
-          }
-        },
-        "in::receive" } );
-    addPadSpec( { "in", { sim::PadSpec::Flag::CAN_INPUT }, {} } );
-    addPadSpec( { "out", { sim::PadSpec::Flag::CAN_OUTPUT }, {} } );
-  }
-
-  void startActivity( sim::Instance &instance, sim::Activity &activity ) override {
-    
-    auto duty_cycle = instance.parameter<double>( "duty_cycle" ).value_or( 2.0 );
-    sim::Clock::duration interval { static_cast<sim::Clock::duration::rep>( sim::Clock::period::den / duty_cycle ) };
-    while( activity.state() == sim::Activity::State::RUN ) {
-      instance.pad( "out" );
-      activity.waitFor( interval );
-    }
-  }
-
-};
-
 TEST_F( SimulatorTest, instancing ) {
-  sim::Simulator::getInstance().addModel<LoopbackModel>();
+  sim::Simulator::getInstance().addModel<EmptyModel>();
 
-  EXPECT_TRUE( sim::Simulator::getInstance().model( "LoopbackModel" ) );
+  EXPECT_TRUE( sim::Simulator::getInstance().model( "EmptyModel" ) );
+
+  sim::Simulation sim;
+
+  sim.spawnInstance( "EmptyModel", "inst" );
+
+  sim.setState( sim::Simulation::State::RUN );
 }
 
+/**
+ * Fixture for testing the simulator which resets the global simulator instance for each test.
+ */
+struct SimulationTest : testing::Test {
+  class LoopbackModel : public sim::Model {
+  public:
+    LoopbackModel() : Model( "LoopbackModel" ) {
+      addActivitySpec( {"sink",
+          []( sim::Instance &instance, sim::Activity &activity ) {
+            while ( activity.state() == sim::Activity::State::RUN ) {
+              activity.padReceive( "in" );
+            }
+          },
+          "in::receive"} );
+      addPadSpec( {"in", {sim::PadSpec::Flag::CAN_INPUT}, {}} );
+      addPadSpec( {"out", {sim::PadSpec::Flag::CAN_OUTPUT}, {}} );
+    }
+
+    void startActivity( sim::Instance &instance, sim::Activity &activity ) override {
+      auto duty_cycle = instance.parameter<double>( "duty_cycle" ).value_or( 2.0 );
+      sim::Clock::duration interval{
+          static_cast<sim::Clock::duration::rep>( sim::Clock::period::den / duty_cycle )};
+      while ( activity.state() == sim::Activity::State::RUN ) {
+        instance.pad( "out" );
+        activity.waitFor( interval );
+      }
+    }
+  };
+
+  void SetUp() override {
+    auto &simulator = sim::Simulator::getInstance();
+    simulation.reset();
+    simulator.reset();
+    simulator.addModel<LoopbackModel>();
+    simulation = std::make_shared<sim::Simulation>();
+  }
+
+  void TearDown() override {
+  }
+
+  std::shared_ptr<sim::Simulation> simulation;
+};
+
+TEST_F( SimulationTest, instance_basic ) {
+  simulation->spawnInstance( "LoopbackModel", "looper" );
+
+  simulation->setState( sim::Simulation::State::RUN );
+}
